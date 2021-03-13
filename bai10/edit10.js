9d5ca103dab6a43afc41d734e30595e1a53a88ca
dphong08 <dphongg199x@gmail.com>
2021-03-13 16:58:53 +0700

    up

--- bai10/edit10.js
+++ bai10/edit10.js
@@ -6,27 +6,27 @@ if (sodien > 0) {
     giatien = sodien * 1678;
     hoadon = giatien * 0.1 + giatien;
     alert(hoadon + "vnđ");
-} else if (sodien <= 100) {
+    } else if (sodien <= 100) {
     giatien = 83900 + (sodien - 50) * 1734;
     hoadon = giatien * 0.1 + giatien;
     alert(hoadon + "vnđ");
-} else if (sodien <= 200) {
+    } else if (sodien <= 200) {
     giatien = 83900 + 86700 + (sodien - 100) * 2014;
     hoadon = giatien * 0.1 + giatien;
     alert(hoadon + "vnđ");
-} else if (sodien <= 300) {
+    } else if (sodien <= 300) {
     giatien = 83900 + 86700 + 201400 + (sodien - 200) * 2536;
     hoadon = giatien * 0.1 + giatien;
     alert(hoadon + "vnđ");
-} else if (sodien <= 400) {
+    } else if (sodien <= 400) {
     giatien = 83900 + 86700 + 201400 + 253600 + (sodien - 300) * 2834;
     hoadon = giatien * 0.1 + giatien;
     alert(hoadon + "vnđ");
-} else if (sodien > 400) {
+    } else if (sodien > 400) {
     giatien = 83900 + 86700 + 201400 + 253600 + 283400 + (sodien - 400) * 2927;
     hoadon = giatien * 0.1 + giatien;
     alert(hoadon + "vnđ");
-}
+    }
 } else {
     alert("Nhập số điện > 0");
 }
